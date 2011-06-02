74ca968201f0a342b553a3fa1975d80f7559cf50
Judy Panicker <judy.panicker@gmail.com>
2011-06-01 20:06:15 -0400

    modified login page

--- login_page.php
+++ login_page.php
@@ -113,7 +113,7 @@ html_page_top1();
 html_page_top2a();
 
 if( $f_error || $f_cookie_error ) {
-	echo '<div class="important-msg">';
+	echo '<div class="msg-error ui-state-highlight ui-corner-all">';
 	echo '<ul>';
 
 	# Display short greeting message
@@ -132,53 +132,32 @@ if( $f_error || $f_cookie_error ) {
 ?>
 
 <!-- Login Form BEGIN -->
+<div id="login-container">
 <div id="login-div" class="form-container">
+
+
+
+
+
 	<form id="login-form" method="post" action="login.php">
 		<fieldset>
-			<legend><span><?php echo lang_get( 'login_title' ) ?></span></legend>
-			<?php
-			if ( !is_blank( $f_return ) ) {
-				echo '<input type="hidden" name="return" value="', string_html_specialchars( $f_return ), '" />';
-			}
-			# CSRF protection not required here - form does not result in modifications
-			echo '<ul id="login-links">';
-
-			if ( ON == config_get( 'allow_anonymous_login' ) ) {
-				echo '<li><a href="login_anon.php?return=' . string_url( $f_return ) . '">' . lang_get( 'login_anonymously' ) . '</a></li>';
-			}
-
-			if ( ( ON == config_get_global( 'allow_signup' ) ) &&
-				( LDAP != config_get_global( 'login_method' ) ) &&
-				( ON == config_get( 'enable_email_notification' ) )
-			) {
-				echo '<li><a href="signup_page.php">', lang_get( 'signup_link' ), '</a></li>';
-			}
-			# lost password feature disabled or reset password via email disabled -> stop here!
-			if ( ( LDAP != config_get_global( 'login_method' ) ) &&
-				( ON == config_get( 'lost_password_feature' ) ) &&
-				( ON == config_get( 'send_reset_password' ) ) &&
-				( ON == config_get( 'enable_email_notification' ) ) ) {
-				echo '<li><a href="lost_pwd_page.php">', lang_get( 'lost_password_link' ), '</a></li>';
-			}
-			?>
-			</ul>
-			<div class="field-container <?php echo helper_alternate_class_no_attribute(); ?>">
+			<div class="field-container">
 				<label for="username"><span><?php echo lang_get( 'username' ) ?></span></label>
-				<span class="input"><input id="username" type="text" name="username" size="32" maxlength="<?php echo USERLEN;?>" value="<?php echo string_attribute( $f_username ); ?>" class="<?php echo $t_username_field_autofocus ?>" /></span>
+				<span class="input"><input id="username" type="text" name="username" size="40" maxlength="<?php echo USERLEN;?>" value="<?php echo string_attribute( $f_username ); ?>" class="<?php echo $t_username_field_autofocus ?>" /></span>
 				<span class="label-style"></span>
 			</div>
-			<div class="field-container <?php echo helper_alternate_class_no_attribute(); ?>">
+			<div class="field-container">
 				<label for="password"><span><?php echo lang_get( 'password' ) ?></span></label>
-				<span class="input"><input id="password" type="password" name="password" size="16" maxlength="<?php echo PASSLEN;?>" class="<?php echo $t_password_field_autofocus ?>" /></span>
+				<span class="input"><input id="password" type="password" name="password" size="40" maxlength="<?php echo PASSLEN;?>" class="<?php echo $t_password_field_autofocus ?>" /></span>
 				<span class="label-style"></span>
 			</div>
-			<div class="field-container <?php echo helper_alternate_class_no_attribute(); ?>">
+			<div class="field-container">
 				<label for="remember-login"><span><?php echo lang_get( 'save_login' ) ?></span></label>
-				<span class="input"><input id="remember-login" type="checkbox" name="perm_login" <?php echo ( $f_perm_login ? 'checked="checked" ' : '' ) ?>/></span>
+				<span class="input check-box"><input id="remember-login" type="checkbox" name="perm_login" <?php echo ( $f_perm_login ? 'checked="checked" ' : '' ) ?>/>Yes please</span>
 				<span class="label-style"></span>
 			</div>
 			<?php if ( $t_session_validation ) { ?>
-			<div class="field-container <?php echo helper_alternate_class_no_attribute(); ?>">
+			<div class="field-container">
 				<label id="secure-session-label" for="secure-session"><span><?php echo lang_get( 'secure_session' ) ?></span></label>
 				<span class="input">
 					<input id="secure-session" type="checkbox" name="secure_session" <?php echo ( $t_default_secure_session ? 'checked="checked" ' : '' ) ?>/>
@@ -187,9 +166,35 @@ if( $f_error || $f_cookie_error ) {
 				<span class="label-style"></span>
 			</div>
 			<?php } ?>
-			<span class="submit-button"><input type="submit" class="button" value="<?php echo lang_get( 'login_button' ) ?>" /></span>
+
 		</fieldset>
+
+			<link rel="stylesheet" type="text/css" href="../plugins/jquery-ui-1.8.13.custom/css/custom-theme/jquery-ui-1.8.13.custom.css" media="all" />
+
+
+					<ul class="actions">
+					<li><input type="submit" value="<?php echo lang_get( 'login_button' ) ?>" /></li>
+					<li><input type="submit" value="<?php echo lang_get( 'lost_password_link' ) ?>" /></li>
+					<li><input type="submit" value="<?php echo lang_get( 'signup_link' ) ?>" /></li>
+					<li><input type="submit" value="<?php echo lang_get( 'login_anonymously' ) ?>" /></li>
+			</ul>
+
+
 	</form>
+</div>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js" />
+<script type="text/javascript">
+
+	$(function() {
+		$( "input:submit", ".actions" ).button();
+
+	});
+
+</script>
+
+
+
+
 </div>
 
 <?php
@@ -256,7 +261,7 @@ if ( config_get_global( 'admin_checks' ) == ON ) {
 		}
 	}
 	if( count( $t_warnings ) > 0 ) {
-		echo '<div class="important-msg">';
+		echo '<div class="msg-error ui-state-error ui-corner-all">';
 		echo '<ul>';
 		foreach( $t_warnings AS $t_warning ) {
 			echo '<li>' . $t_warning . '</li>';
@@ -267,3 +272,5 @@ if ( config_get_global( 'admin_checks' ) == ON ) {
 } # if 'admin_checks'
 
 html_page_bottom1a( __FILE__ );
+
+
